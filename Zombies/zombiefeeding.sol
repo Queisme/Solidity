a931f1e8c028f5d4940be31770da719c31412111
Queisme <queiskey@gmail.com>
2024-10-29 01:59:43 -0500

    lesson2-chapter8-done

--- Zombies/zombiefeeding.sol
+++ Zombies/zombiefeeding.sol
@@ -23,6 +23,10 @@ contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
+        _targetDna = _targetDna % dnaModulus;
+        uint newDna = (myZombie.dna + _targetDna) / 2;
+        _createZombie("NoName", newDna);
+
     }
 
 }
